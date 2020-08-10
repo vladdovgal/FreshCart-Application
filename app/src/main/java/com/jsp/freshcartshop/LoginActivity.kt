12a8fe085e77f9808f1444921dffbcb5f9fdb76e
vladdovgal <vladdovgal@gmail.com>
2020-08-10 23:08:58 +0300

    login page

---
+++ app/src/main/java/com/jsp/freshcartshop/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.jsp.freshcartshop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // set fonts
+        welcomeLogo.typeface = ResourcesCompat.getFont(this, R.font.fortemt)
+        val poppinsTypeface = ResourcesCompat.getFont(this, R.font.poppins_medium)
+        etUsername.typeface = poppinsTypeface
+        inputUsername.typeface = poppinsTypeface
+        etPassword.typeface = poppinsTypeface
+        inputPassword.typeface = poppinsTypeface
+    }
+
+}
--- app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
+++ app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.jsp.freshcartshop
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        homeNavButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
