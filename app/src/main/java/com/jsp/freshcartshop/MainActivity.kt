1261532ccd67e5c19459f2c0cb62d987c2f9bbc1
MarianDutchyn <marian.dutchyn@ukr.net>
2020-08-11 18:28:47 +0300

    remove declaration of global UI variables, extract dimensions to file

--- app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
+++ app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
@@ -7,19 +7,14 @@ import android.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var arrowBack: ImageButton
-    private lateinit var navMenu: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        arrowBack = findViewById(R.id.arrowBack)
-        navMenu = findViewById(R.id.navMenu)
-
         navMenu.setOnClickListener{
-            val popupMenu: PopupMenu = PopupMenu(this, navMenu)
+            val popupMenu = PopupMenu(this, navMenu)
             popupMenu.menuInflater.inflate(R.menu.nav_menu, popupMenu.menu)
             popupMenu.show()
         }
