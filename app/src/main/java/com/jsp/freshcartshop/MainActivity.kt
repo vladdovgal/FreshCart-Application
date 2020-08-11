4e7f98eab14f18eba90bbb56e0ed99fffec20843
vladdovgal <vladdovgal@gmail.com>
2020-08-11 21:51:47 +0300

    resolve merge conflicts in MainActivity.kt and styles.xml

--- app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
+++ app/src/main/java/com/jsp/freshcartshop/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.jsp.freshcartshop
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_main.*
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
