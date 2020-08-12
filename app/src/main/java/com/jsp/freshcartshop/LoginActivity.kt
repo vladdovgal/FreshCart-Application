a4e3184fa3cc4a3fc9055bc1637e1a058ec95563
vladdovgal <vladdovgal@gmail.com>
2020-08-12 13:00:23 +0300

    solve problems with fonts import

--- app/src/main/java/com/jsp/freshcartshop/LoginActivity.kt
+++ app/src/main/java/com/jsp/freshcartshop/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.jsp.freshcartshop
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,18 +10,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // set fonts
-        welcomeLogo.typeface = ResourcesCompat.getFont(this, R.font.fortemt)
-        val poppinsTypeface = ResourcesCompat.getFont(this, R.font.poppins_medium)
-        etUsername.typeface = poppinsTypeface
-        inputUsername.typeface = poppinsTypeface
-        etPassword.typeface = poppinsTypeface
-        inputPassword.typeface = poppinsTypeface
-
         signInButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
