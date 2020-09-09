060fff96e872d6a4b3047e07fe9476c1a1f496a9
MarianDutchyn <marian.dutchyn@ukr.net>
2020-09-09 15:03:58 +0300

    minor changes

--- app/src/main/java/com/jsp/freshcartshop/view/fragment/BaseFragment.kt
+++ app/src/main/java/com/jsp/freshcartshop/view/fragment/BaseFragment.kt
@@ -1,12 +1,10 @@
 package com.jsp.freshcartshop.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.jsp.freshcartshop.R
+import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
